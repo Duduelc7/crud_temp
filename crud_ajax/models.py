e46597055e474a4b24b0ef2debafc1b5252e84b6
Duduelc7 <eduardocordeiro1906@gmail.com>
2022-10-23 20:16:23 -0300

    ajax teste

--- crud_ajax/models.py
+++ crud_ajax/models.py
@@ -1,6 +1,7 @@
 from msilib.schema import ListView
 from unicodedata import name
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
 
@@ -9,3 +10,10 @@ class Conta(models.Model):
     id = models.BigAutoField(primary_key=True)
     desc = models.CharField(max_length=200,verbose_name='Descrição')
     fornecedor = models.CharField(max_length=255)
+
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=100)
+    num_stars = models.IntegerField()
+    artist = models.CharField(max_length=200)
--- crud_ajax/views.py
+++ crud_ajax/views.py
@@ -2,7 +2,8 @@ from tkinter.messagebox import NO
 from django.shortcuts import render
 from django.views.generic import *
 from django.http import JsonResponse
-from crud_ajax.models import Conta
+from matplotlib.artist import Artist
+from crud_ajax.models import Conta, Album
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -33,3 +34,31 @@ class CreateConta(View):
         }
 
         return JsonResponse(data)
+
+class AlbumView(ListView):
+    model = Album
+    template_name = 'crud_ajax/album.html'
+    context_object_name = 'album'
+
+class CreateAlbum(View):
+    def get (self,request):
+        name1  = request.GET.get('name', None)
+        artist1 = request.GET.get('artist', None)
+        num_stars1 = request.GET.get('num_stars', None)
+
+
+
+        obj = Album.objects.create(
+            name = name1,
+            artist = artist1,
+            num_stars = num_stars1
+        )
+
+        user = {'id':obj.id,'name':obj.name,'artist': obj.artist,'num_stars': obj.num_stars}
+
+        data = {
+            'user': user
+        }
+
+        return JsonResponse(data)
+        
